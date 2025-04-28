b2337efea2d08c986ddb48e3c86d78adee36d540
Amon Muhwezi <amon.muhwezik@gmail.com>
2025-04-28 14:56:57 +0300

    gunicorn app

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = app
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
-    host = '0.0.0.0'
+    host = '127.0.0.1'
 
     if os.getenv('FLASK_ENV') == 'production':
         print("🚀 Starting production server with Waitress...")
