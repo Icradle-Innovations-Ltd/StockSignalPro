7aef53c9da9b21643bc955f6de68592ffb3e1e6e
amon.muhwezik <amon.muhwezik@gmail.com>
2025-04-28 14:32:45 +0000

    procfile

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,11 +7,11 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
+      pkgs.python312
+      pkgs.python312Packages.pip
+      pkgs.gunicorn  
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
-  ];
 
   # Sets environment variables in the workspace
   env = {};
